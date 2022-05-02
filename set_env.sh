8ce96b09c00fcc0f0ab7a66066b8c71b1d73f440
Javi ME <jmespartero@gmail.com>
2022-05-02 22:37:28 +0200

    Creating cluster

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=javier
+export POSTGRES_PASSWORD=admin123
+export POSTGRES_HOST=project3.c37vltnulqlt.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=microservices-project-127366014667-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
