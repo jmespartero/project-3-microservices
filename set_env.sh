7d2b9a15722a2c1df08f649efa0337a246eafbf8
jmespartero <40332824+jmespartero@users.noreply.github.com>
2022-06-19 21:18:57 +0200

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=javier
-export POSTGRES_PASSWORD=admin123
+export POSTGRES_USERNAME=<>
+export POSTGRES_PASSWORD=<>
 export POSTGRES_HOST=udagram.c37vltnulqlt.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=udagram-127366014667-dev
